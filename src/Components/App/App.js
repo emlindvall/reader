d5d9425344e275b7b0b218c31645319a7e26fdbe
Em Lindvall <122124478+emlindvall@users.noreply.github.com>
2023-08-01 22:35:32 -0500

    Fix whatever got messed up with the local storage refresh, plus refactor StoryDetails JSX to properly pass formattedDate function

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -10,9 +10,9 @@ import './App.css';
 const App = () => {
   const [feed, setFeed] = useState([]);
   const [resultsFeed, setResultsFeed] = useState([]);
-  // const [query, setQuery] = useState('');
 
   useEffect(() => {
+    // Function to fetch data from the API and set feed state.
     const fetchData = async () => {
       try {
         const data = await getStories();
@@ -20,39 +20,46 @@ const App = () => {
           ...story,
           newId: nanoid(),
         }));
-        localStorage.setItem('feed', JSON.stringify(newFeed));
         setFeed(newFeed);
+        localStorage.setItem('feed', JSON.stringify(newFeed));
       } catch (error) {
         console.error('Error fetching stories:', error);
       }
     };
 
-    fetchData();
+    // Check if feed data exists in localStorage, if so, load it into state.
+    const storedFeed = localStorage.getItem('feed');
+    if (storedFeed) {
+      setFeed(JSON.parse(storedFeed));
+    } else {
+      fetchData();
+    }
   }, []);
 
   const handleSearch = (incomingQuery) => {
-    // setQuery(incomingQuery);
     const resultsFeed = feed.filter((story) => {
-        return (
-          story.title.toLowerCase().includes(incomingQuery) ||
-          (story.description !== null && story.description.toLowerCase().includes(incomingQuery)) ||
-          (story.author !== null && story.author.toLowerCase().includes(incomingQuery))
-        );
+      return (
+        story.title.toLowerCase().includes(incomingQuery) ||
+        (story.description !== null && story.description.toLowerCase().includes(incomingQuery)) ||
+        (story.author !== null && story.author.toLowerCase().includes(incomingQuery))
+      );
     });
     setResultsFeed(resultsFeed);
   };
 
   const reloadFeed = () => {
-    setResultsFeed('');
-    // setQuery('');
-    setFeed(JSON.parse(localStorage.getItem('feed')));
-  }
+    setResultsFeed([]);
+    const storedFeed = localStorage.getItem('feed');
+    if (storedFeed) {
+      setFeed(JSON.parse(storedFeed));
+    }
+  };
 
   return (
     <div className="display-container">
-      <Header handleSearch={handleSearch}/>
+      <Header handleSearch={handleSearch} />
       <Routes>
-        <Route path="/" element={<Feed feed={feed} resultsFeed={resultsFeed} reloadFeed={reloadFeed}/>} />
+        <Route path="/" element={<Feed feed={feed} resultsFeed={resultsFeed} reloadFeed={reloadFeed} />} />
         <Route path="/:id" element={<StoryDetails feed={feed} />} />
       </Routes>
     </div>
